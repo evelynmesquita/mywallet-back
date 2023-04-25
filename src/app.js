251c301b2d48afea4c6959927d1065f8b575719f
Evelyn <evelynbandeiramesquita@gmail.com>
2023-04-25 14:28:14 -0300

    alter port

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors"
 import router from "./routes/index.routes.js";
-import dotenv from "dotenv"
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router)
 
-const PORT = 5000;
-app.listen(process.env.PORT, () => console.log("Servidor rodando na porta" + `${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Servidor rodando na porta" + ${port}`))
